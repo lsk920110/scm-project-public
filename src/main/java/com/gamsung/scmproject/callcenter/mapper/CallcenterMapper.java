6f9ea98825c7a9b29e76252a7dec19a5ca3c20b1
lsk <lsk@rowem.com>
2023-06-23 16:55:46 +0900

    callcenter 제작중

--- src/main/java/com/gamsung/scmproject/callcenter/mapper/CallcenterMapper.java
+++ src/main/java/com/gamsung/scmproject/callcenter/mapper/CallcenterMapper.java
@@ -3,12 +3,15 @@ package com.gamsung.scmproject.callcenter.mapper;
 import com.gamsung.scmproject.callcenter.vo.HistoryInfoVo;
 import com.gamsung.scmproject.callcenter.vo.OrderInfoWithCallHistoryVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
 
 @Mapper
 public interface CallcenterMapper {
-    OrderInfoWithCallHistoryVo selectOrderInfoWithCallHistory(Long vendorId, String criteria, String content);
+    OrderInfoWithCallHistoryVo selectOrderInfoWithCallHistory(@Qualifier("vendorId") Long vendorId,
+                                                              @Qualifier("criteria") String criteria,
+                                                              @Qualifier("content") String content);
 
     List<HistoryInfoVo> selectCallHistoryList(Long orderId);
 }
